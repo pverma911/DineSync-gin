8e296cc5f022f94ca331ad15eb684e0bddb1b158
pverma911 <verma.pranshu689@gmail.com>
2025-05-17 10:44:56 +0530

    update() : implemented database initialization with AutoMigration

---
+++ internal/config/db.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"log"
+
+	"github.com/pverma911/go-gin-tonic/internal/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitializeDb() *gorm.DB {
+	dsn := "host=localhost user=postgres password=12345 dbname=go_gin_tonic port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if(err != nil){
+		log.Fatalf("Error while initializing DB %s",err.Error())
+	}
+
+	return db
+}
+
+func CreateOrUpdateTables(db *gorm.DB){
+	db.AutoMigrate(&model.User{})
+}
--- internal/model/user.go
+++ internal/model/user.go
@@ -1,6 +1,13 @@
 package model
 
+import "time"
+
 type User struct {
-    ID   int    `json:"id"`
-    Name string `json:"name"`
+	ID        uint       `json:"id"`
+	Name      string     `json:"name"`
+	Age       int        `json:"age"`
+	Birthday  *time.Time // if pointer is not used, then value can't be null, it will have default go time
+	Is_Active bool       `json:"isActive"`
+	CreatedAt time.Time  `gorm:"->" json:"createdAt"`// Automatically managed by GORM for creation time
+	UpdatedAt time.Time  `gorm:"->" json:"updatedAt"`// Automatically managed by GORM for update time
 }
