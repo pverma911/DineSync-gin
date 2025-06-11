2217af21a07c39562760134ee023ce37b9e9e7cc
pverma911 <verma.pranshu689@gmail.com>
2025-06-11 15:14:01 +0530

    update(): comment on server start

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pverma911/go-gin-tonic/internal/config"
 	"github.com/pverma911/go-gin-tonic/internal/router"
 )
@@ -10,4 +12,5 @@ func main() {
     config.CreateOrUpdateTables(db)
     r := router.SetupRouter(db)
     r.Run("127.0.0.1:8080")
+    fmt.Println("Server started")
 }
